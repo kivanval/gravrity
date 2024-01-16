2fb277535d96fb0415584a1743eb60431937b996
Ivan Kyrylov <ivan.kyrylov@intellias.com>
2024-01-16 04:02:56 +0200

    Add base structure and tests Add framework for AvroSourceSet

---
+++ plugin/src/test/groovy/io/sofa/gradle/AvrohuggerPluginFunctionalTest.groovy
@@ -0,0 +1,42 @@
+package io.sofa.gradle
+
+import org.gradle.testkit.runner.GradleRunner
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.TempDir
+
+
+class AvrohuggerPluginFunctionalTest extends Specification {
+	@Shared @TempDir File projectDir
+	@Shared File buildFile
+	@Shared File settingsFile
+
+	def setupSpec() {
+		buildFile = new File(projectDir, "build.gradle")
+		buildFile << """
+           plugins {
+               id('io.sofa.avrohugger')
+           }
+           sourceSets {
+               main {
+                   avro {}
+               }
+           }
+           """
+		settingsFile = new File(projectDir, "settings.gradle")
+		settingsFile << ""
+	}
+
+	def "can run task"() {
+		when:
+		GradleRunner.create()
+				.forwardOutput()
+				.withPluginClasspath()
+				.withArguments("compileJava")
+				.withProjectDir(projectDir)
+				.build()
+
+		then:
+		noExceptionThrown()
+	}
+}
