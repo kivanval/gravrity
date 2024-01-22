0c9d76ee950c7bd63d538d8746c3cad3206e27e5
miraclesnake <Leraka833@gmail.com>
2024-01-22 17:40:18 +0200

    licence header

--- spotless/copyright.java
+++ spotless/copyright.java
@@ -13,18 +13,3 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package io.github.kivanval.gradle;
-
-import java.util.Objects;
-import lombok.experimental.ExtensionMethod;
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
-
-@ExtensionMethod(Objects.class)
-public class AvrohuggerPlugin implements Plugin<Project> {
-	@Override
-	public void apply(final Project project) {
-		project.getPluginManager().apply(AvrohuggerBasePlugin.class);
-		// TODO
-	}
-}
