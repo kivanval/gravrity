b57dd765b9611c89fb53155447a9d9484945ca21
miraclesnake <Leraka833@gmail.com>
2024-01-23 20:57:37 +0200

    add primitive avro scala types

--- plugin/src/main/groovy/io/github/kivanval/gradle/DefaultAvrohuggerExtension.groovy
+++ plugin/src/main/groovy/io/github/kivanval/gradle/DefaultAvrohuggerExtension.groovy
@@ -15,8 +15,19 @@ limitations under the License.
 */
 package io.github.kivanval.gradle
 
+import avrohugger.types.AvroScalaBooleanType
+import avrohugger.types.AvroScalaBytesType
+import avrohugger.types.AvroScalaNullType
 import avrohugger.types.AvroScalaNumberType
+import avrohugger.types.AvroScalaStringType
+import avrohugger.types.ScalaBoolean$
+import avrohugger.types.ScalaByteArray$
+import avrohugger.types.ScalaDouble$
+import avrohugger.types.ScalaFloat$
 import avrohugger.types.ScalaInt$
+import avrohugger.types.ScalaLong$
+import avrohugger.types.ScalaNull$
+import avrohugger.types.ScalaString$
 import groovy.transform.CompileStatic
 import io.github.kivanval.gradle.format.AvroSourceFormat
 import io.github.kivanval.gradle.format.SpecificRecord
@@ -36,6 +47,13 @@ class DefaultAvrohuggerExtension implements AvrohuggerExtensionOperations {
 	final Provider<AvroSourceFormat> specificRecord
 
 	final Provider<AvroScalaNumberType> scalaInt
+	final Provider<AvroScalaNumberType> scalaLong
+	final Provider<AvroScalaNumberType> scalaFloat
+	final Provider<AvroScalaNumberType> scalaDouble
+	final Provider<AvroScalaBooleanType> scalaBoolean
+	final Provider<AvroScalaStringType> scalaString
+	final Provider<AvroScalaNullType> scalaNull
+	final Provider<AvroScalaBytesType> scalaByteArray
 
 	private final ObjectFactory objects
 
@@ -49,6 +67,13 @@ class DefaultAvrohuggerExtension implements AvrohuggerExtensionOperations {
 		this.specificRecord = getProperty(AvroSourceFormat, objects.<SpecificRecord>newInstance(SpecificRecord))
 
 		this.scalaInt = getProperty(AvroScalaNumberType, ScalaInt$.MODULE$)
+		this.scalaLong = getProperty(AvroScalaNumberType, ScalaLong$.MODULE$)
+		this.scalaFloat = getProperty(AvroScalaNumberType, ScalaFloat$.MODULE$)
+		this.scalaDouble = getProperty(AvroScalaNumberType, ScalaDouble$.MODULE$)
+		this.scalaBoolean = getProperty(AvroScalaBooleanType, ScalaBoolean$.MODULE$)
+		this.scalaString = getProperty(AvroScalaStringType, ScalaString$.MODULE$)
+		this.scalaNull = getProperty(AvroScalaNullType, ScalaNull$.MODULE$)
+		this.scalaByteArray = getProperty(AvroScalaBytesType, ScalaByteArray$.MODULE$)
 	}
 
 	private <T> Property<T> getProperty(Class<T> target, T value) {
