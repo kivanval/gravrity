e0dbfaff9e36fdd20c14e2dfb7531e57dda158fd
miraclesnake <94176275+miraclesnake@users.noreply.github.com>
2024-01-31 22:47:01 +0200

    Added types test (#6)
    * added test class for primitives
    
    * changed formatting
    
    * changed formatting
    
    * added test class for each type
    
    * changed test name

---
+++ plugin/src/test/groovy/io/github/kivanval/gradle/PrimitiveAvroScalaTypesTest.groovy
@@ -0,0 +1,44 @@
+/*
+Copyright 2024 Kyrylov Ivan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package io.github.kivanval.gradle
+
+import avrohugger.types.*
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class PrimitiveAvroScalaTypesTest extends Specification {
+	def "primitive types matches to values in avrohugger library"() {
+		given:
+		def project = ProjectBuilder.builder().build()
+
+		when:
+		project.pluginManager.with {
+			apply(AvrohuggerPlugin)
+		}
+
+		then:
+		with(project.avrohugger) {
+			scalaInt == ScalaInt$.MODULE$
+			scalaLong == ScalaLong$.MODULE$
+			scalaFloat == ScalaFloat$.MODULE$
+			scalaDouble == ScalaDouble$.MODULE$
+			scalaBoolean == ScalaBoolean$.MODULE$
+			scalaString == ScalaString$.MODULE$
+			scalaNull == ScalaNull$.MODULE$
+			scalaByteArray == ScalaByteArray$.MODULE$
+		}
+	}
+}
