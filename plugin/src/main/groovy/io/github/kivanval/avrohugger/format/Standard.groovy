0aec2bed803b6f8edda69fd80fdf8d99df03c21b
kivan <kivanval@gmail.com>
2024-07-05 01:03:24 +0300

    Update formatting

--- plugin/src/main/groovy/io/github/kivanval/avrohugger/format/Standard.groovy
+++ plugin/src/main/groovy/io/github/kivanval/avrohugger/format/Standard.groovy
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Kyrylov Ivan
+Copyright 2024 Ivan Kyrylov
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
