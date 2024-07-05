e359446ad83794cfe05a24093ba07441fd17a5c7
Ivan Kyrylov <81202331+kivanval@users.noreply.github.com>
2024-07-05 12:25:51 +0300

    Modular plugin refactoring (#24)
    * Refactor structure
    
    * Rename to "gravrity"
    
    * Update sandbox
    
    ---------
    
    Co-authored-by: Ivan Kyrylov <ivan.kyrylov@intellias.com>

---
+++ plugin/base/src/functionalTest/groovy/io/github/kivanval/gravrity/test/AvroExtractFunctionalTest.groovy
@@ -0,0 +1,81 @@
+/*
+Copyright 2024 Ivan Kyrylov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package io.github.kivanval.gravrity.test
+
+import io.github.kivanval.gravrity.util.TestUtils
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+import org.gradle.internal.impldep.com.google.common.io.Resources
+import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.Specification
+import spock.lang.TempDir
+
+class AvroExtractFunctionalTest extends Specification {
+  @TempDir
+  Path projectDir
+  Path buildFile
+  Path extractedOutputDir
+
+  def setup() {
+    extractedOutputDir = Files
+      .createDirectories(projectDir.resolve("build/extracted/sources/gravrity/avro/main"))
+    buildFile = projectDir.resolve("build.gradle")
+    buildFile << TestUtils.resource("sample.gradle")
+  }
+
+  def "task extracts nested archive with avro files"() {
+    given:
+    def lib = Files.createDirectories(projectDir.resolve("lib"))
+    def localArchive = lib.resolve('nestedSample.zip')
+    def resPath = Path.of(Resources.getResource('nestedSample.zip').toURI())
+    localArchive << Files.readAllBytes(resPath)
+    buildFile << '''
+          dependencies {
+              avro files('lib/nestedSample.zip')
+          }
+        '''
+
+    when:
+    def buildResult = TestUtils
+      .gradleRunner(projectDir, gradleVersion, "extractAvro")
+      .build()
+
+
+    then:
+    def extractedFileNames = new HashSet<Path>()
+    Files.walkFileTree(extractedOutputDir, new SimpleFileVisitor<Path>() {
+        @Override
+        FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+          extractedFileNames.add(extractedOutputDir.relativize(file))
+          return FileVisitResult.CONTINUE
+        }
+      })
+    buildResult.task(":extractAvro").outcome == TaskOutcome.SUCCESS
+    extractedFileNames.size() == 6
+    extractedFileNames.contains(Path.of('example', 'user_activity.avsc'))
+    extractedFileNames.contains(Path.of('example', 'yelp_review.avsc'))
+    extractedFileNames.contains(Path.of('example', 'truck_events.avsc'))
+    extractedFileNames.contains(Path.of('user_profile.avsc'))
+    extractedFileNames.contains(Path.of('user_activity.avsc'))
+    extractedFileNames.contains(Path.of('page_view.avsc'))
+
+    where:
+    gradleVersion << TestUtils.GRADLE_VERSIONS
+  }
+}
---
+++ plugin/scala/src/test/groovy/io/github/kivanval/gravrity/scala/plugin/GravrityScalaPluginTest.groovy
@@ -0,0 +1,72 @@
+/*
+Copyright 2024 Ivan Kyrylov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package io.github.kivanval.gravrity.scala.plugin
+
+import java.nio.file.Paths
+import org.gradle.api.tasks.ScalaSourceDirectorySet
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class GravrityScalaPluginTest extends Specification {
+
+  def "plugin should have avro default settings in sourceSets"() {
+    given:
+    def project = ProjectBuilder.builder().build()
+    def buildDir = project.layout.buildDirectory.asFile.get()
+      .toString()
+    def generatedSourceDirs = Paths.get(buildDir, "generated/sources/gravrity/scala/$sourceSetName")
+      .toString()
+    def srcDir = Paths.get(project.projectDir.toString(), "src/$sourceSetName/avro")
+      .toString()
+    def extractedSrcDir = Paths.get(buildDir, "/extracted/sources/gravrity/avro/$sourceSetName").toString()
+
+    when:
+    project.pluginManager.apply(GravrityScalaPlugin)
+
+    then:
+    def sourceSet = project.sourceSets.getByName(sourceSetName)
+
+    sourceSet.avro.srcDirs.collect { it.toString() }.contains(srcDir)
+    sourceSet.avro.srcDirs.collect { it.toString() }.contains(extractedSrcDir)
+    sourceSet.avro.destinationDirectory.get().toString() == generatedSourceDirs
+    sourceSet.output.generatedSourcesDirs.collect { it.toString() }.contains(generatedSourceDirs)
+
+    where:
+    sourceSetName << ['main', 'test']
+  }
+
+  def "scala sourceSourceSet contains generated sources"() {
+    given:
+    def project = ProjectBuilder.builder().build()
+    def buildDir = project.layout.buildDirectory.asFile.get()
+      .toString()
+    def generatedSourceDirs = Paths.get(buildDir, "generated/sources/gravrity/scala/$sourceSetName")
+      .toFile()
+
+    when:
+    project.pluginManager.with {
+      apply(GravrityScalaPlugin)
+    }
+
+    then:
+    def sourceSet = project.sourceSets.getByName(sourceSetName)
+    def scalaSourceSet = sourceSet.extensions.getByType(ScalaSourceDirectorySet)
+    scalaSourceSet.srcDirs.contains(generatedSourceDirs)
+
+    where:
+    sourceSetName << ['main', 'test']
+  }
+}
