29aad4b6fe5e6d14f63699998cad1c0784ec4ae9
User <viktor.master7@gmail.com>
2024-07-10 00:12:59 +0300

    new extract test

--- plugin/core/src/functionalTest/groovy/io/github/kivanval/gravrity/test/AvroExtractFunctionalTest.groovy
+++ plugin/core/src/functionalTest/groovy/io/github/kivanval/gravrity/test/AvroExtractFunctionalTest.groovy
@@ -38,7 +38,7 @@ class AvroExtractFunctionalTest extends Specification {
     buildFile << TestUtils.resource("sample.gradle")
   }
 
-  def "task extracts nested archive with avro files"() {
+  def "task extracts nested archive with avro files with files() configuration"() {
     given:
     def lib = Files.createDirectories(projectDir.resolve("lib"))
     def localArchive = lib.resolve('nestedSample.zip')
@@ -73,6 +73,42 @@ class AvroExtractFunctionalTest extends Specification {
     extractedFileNames.contains(Path.of('user_profile.avsc'))
     extractedFileNames.contains(Path.of('user_activity.avsc'))
     extractedFileNames.contains(Path.of('page_view.avsc'))
+
+    where:
+    gradleVersion << TestUtils.GRADLE_VERSIONS
+  }
+
+  def "task extracts nested archive with avro files with fileTree() configuration"() {
+    given:
+    def libDir1 = Files.createDirectories(projectDir.resolve("lib/directory1"))
+    def libDir2 = Files.createDirectories(projectDir.resolve("lib/directory2"))
+    Files.createFile(libDir1.resolve('sample.avsc'))
+    Files.createFile(libDir2.resolve('sample.avsc'))
+    buildFile << '''
+          dependencies {
+              avro fileTree('lib')
+          }
+        '''
+
+    when:
+    def buildResult = TestUtils
+            .gradleRunner(projectDir, gradleVersion, "extractAvro")
+            .build()
+
+
+    then:
+    def extractedFileNames = new HashSet<Path>()
+    Files.walkFileTree(extractedOutputDir, new SimpleFileVisitor<Path>() {
+      @Override
+      FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        extractedFileNames.add(extractedOutputDir.relativize(file))
+        return FileVisitResult.CONTINUE
+      }
+    })
+    buildResult.task(":extractAvro").outcome == TaskOutcome.SUCCESS
+    extractedFileNames.size() == 2
+    extractedFileNames.contains(Path.of('directory1', 'sample.avsc'))
+    extractedFileNames.contains(Path.of('directory2', 'sample.avsc'))
 
     where:
     gradleVersion << TestUtils.GRADLE_VERSIONS
